62c4870854422a71812c9000147f9ddfe9233d02
Saya <duisekova30011996@gmail.com>
2021-05-20 16:20:08 +0600

    levi

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.testim__slider').slick({
+            // speed: 500,
+            // fade: true,
+            // cssEase: 'linear',
+            prevArrow:'<img src="img/prev.png" class="slick-arrow__prev">',
+            nextArrow:'<img src="img/next.png" class="slick-arrow__next">'
+    });
+
+});
